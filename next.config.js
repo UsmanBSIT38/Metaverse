30d139c25a1ee0fc8bf436b155358555b1ed7f55
Usman Ch <114169745+UsmanBSIT38@users.noreply.github.com>
2023-08-03 14:03:16 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: false,
+    appDir: true,
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
